5d740bbe897d91173353bc53c6678664509c00f0
Connor Hamilton <clyhamilton@gmail.com>
2022-11-18 15:54:23 -0500

    51% completed; Still working with DOM

--- signup.js
+++ signup.js
@@ -1,3 +1,14 @@
-function createUser(this){
+function createUser(){
+    let firstName = document.getElementById("fname");
+    let lastName = document.getElementById("lname");
+    let email = document.getElementById("email");
+
+    let userArray = [firstName, lastName, email];
+
+    for(let i = 0; i < userArray.length; i++){
+        if(firstName == ""){
+            
+        }
+    }
 
 }

883bd7d2ef047315f814a0dbee3c74efedba61d2
Connor Hamilton <clyhamilton@gmail.com>
2022-11-29 15:12:46 -0500

    CSS done (for now); working on JS

---
+++ login.js
@@ -0,0 +1,7 @@
+//user gets sent to either their profile page or the management page
+//Also include a link to the sign-up page if the user is directed here somehow
+//use an if-statement for the authentication by checking local storage for matches
+
+function login(){
+
+}

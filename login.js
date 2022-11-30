e6cd7c9b1f8eebae745e8a10f27c241a7bc9770d
Connor Hamilton <clyhamilton@gmail.com>
2022-11-30 12:45:07 -0500

    work in progress

--- login.js
+++ login.js
@@ -2,6 +2,12 @@
 //Also include a link to the sign-up page if the user is directed here somehow
 //use an if-statement for the authentication by checking local storage for matches
 
+//Use session storage!!
+
 function login(){
+    let email = localStorage.getItem("email");
+    let password = localStorage.getItem("password");
+
+    return Object.keys()
 
 }

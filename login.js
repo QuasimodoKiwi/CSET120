795494e8bf6639b5116b7f455e6cce6e9267a415
Connor Hamilton <clyhamilton@gmail.com>
2022-12-01 12:56:36 -0500

    Worked out a plan for the forgot password feature. May need help if needed.

--- login.js
+++ login.js
@@ -28,23 +28,46 @@ function signup(){
     window.location = "./signup-page.html";
 }
 
-//Iterate through the users array and look for the email associated with the password.
-//When iterated, create a new element for the label and the textbox using document.body.createElement.
-//Replace the class of the login-input with the new label and textbox.
-//When information is inputted, set the items of the user to have the new password.
-//Reload the page when finished.
+//Forgot Password Steps:
+
+//1: Create a label and button.
+
+//2: Put in a input box and check if a user's email exists within the local storage. 
+
+//3: If the email does exist, enter an input box that allows for a new password to be created.
+
+//4: When iterated, create a new element for the label and the textbox using document.body.createElement.
+//Replace the class of the loginInput with the new label and textbox
+
+//5: Create a confirmation button...
+
+//6: If the confirmation button is clicked, set the items of the user to have the new password.
+
+//7: Reload the page when finished.
 
 function newPassword(){
+    let newLabel = document.createElement("label");
+    newLabel.innerHTML = "form";
+    
+    let newPassword = document.createElement("input");
+    newPassword.setAttribute("type", "text");
+
+
+    // document.getElementsByClassName("loginInput").createElement = "label";
+    
     let users = JSON.parse(localStorage.getItem("users"));
     for(let i = 0; i < users.length; i++){
         if(inputEmail == users.email){
-            let target = document.getElementsByClassName("loginInput");
-            target.forEach(box => {
+            let targets = document.getElementsByClassName("loginInput");
+            targets.forEach(box => {
                 box.remove();
             });
+
         }
         else{
             alert("The email was not found. Please try again.");
         }
        }
+
+    document.body.append(newLabel, newPassword);
 }

bf50774f4335f6c22b63c5b5b3ea0097564c4122
Connor Hamilton <clyhamilton@gmail.com>
2022-12-05 12:27:56 -0500

    switching over to signup-page now.

--- login.js
+++ login.js
@@ -11,10 +11,15 @@ function login(){
 
     let users = JSON.parse(localStorage.getItem("users"));
     for(let i = 0; i < users.length; i++){
-        let user = users[i];
+        let user = users[i]; 
         if(inputEmail == user.email && inputPassword == user.password){
             sessionStorage.setItem("loggedUser", user.name);
-            window.location = "./profile-page.html";
+            if(user.isManager){
+                window.location = "./manager-page.html";
+            }
+            else{
+                window.location = "./profile-page.html";
+            }
             return;
         }
         else{
@@ -27,33 +32,6 @@ function login(){
 function signup(){
     window.location = "./signup-page.html";
 }
-
-//Forgot Password Steps:
-
-//1: 
-// Change the "Login" <h3> element to "Change Password:", the innerHTML of label for password to "New Password:".
-// (optional) change the innerHTML of the Submit button to "Save Password".
-//2:
-//Upon clicking the confirmation button, check if a user's email exists within the local storage. 
-//If the email does exist... 
-    //the user is eligible to create a new password that will replace the value of password inside the users key.
-
-    //If the following two were to happen, set the display of the warningText div to visible, and...
-
-    //... if the password exists, change its innerHTML to "The password already exists. Please try again."
-    
-    //or if the email does NOT exist, change innerHTML to "The email does not exist."
-
-    //or, The email nor the password exists. 
-
-//3: 
-//When iterated, create a new element for the label and the textbox using document.body.createElement.
-
-//6: 
-//If the confirmation button is clicked, set the items of the user to have the new password.
-
-//7: 
-//Reload the page when finished.
 
 function newPassword(){
     document.getElementById("passwordLabel").innerHTML = "New Password: ";

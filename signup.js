b786ef1b0cf1eca4bc9abebab805d7a5a319b85c
Connor Hamilton <clyhamilton@gmail.com>
2022-12-05 13:54:44 -0500

    isManager now works

--- signup.js
+++ signup.js
@@ -3,25 +3,29 @@ function createUser(){
         firstName: document.getElementById("fname").value,
         lastName: document.getElementById("lname").value,
         email: document.getElementById("email").value,
-        password: document.getElementById("password").value
+        password: document.getElementById("password").value,
+        isManager: false
     };
-
-    console.log(password);
-
+    
+    
     if(userInfo.password == ""){
         alert("Please create a password.");
         return;
     }
-
+    
     userInfo.name = userInfo.lastName.toLowerCase() + "_" + userInfo.firstName.toLowerCase();
-
     
     let users = JSON.parse(localStorage.getItem("users"));
+    
+    
+    if(document.getElementById("manager").checked){
+        userInfo.isManager = true;
+    }
     
     if(users == null){
         users = [];
     }
-
+    
     for(let i = 0; i < users.length; i++){
         if(users[i].name == userInfo.name){
             alert("The username is already taken.");
@@ -30,7 +34,7 @@ function createUser(){
     }
     
     users.push(userInfo);
-
+    
     localStorage.setItem("users", JSON.stringify(users));    
     
     console.log(JSON.parse(localStorage.getItem("users")));

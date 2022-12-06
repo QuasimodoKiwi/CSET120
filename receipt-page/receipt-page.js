f860e6dea0675d5cdd56102abe7d9b0860dc3b26
Renzolution <renzolutionn@gmail.com>
2022-12-06 17:57:09 -0500

    Random number generator
    randomizer

--- receipt-page/receipt-page.js
+++ receipt-page/receipt-page.js
@@ -1,14 +1,21 @@
 
 
+window.onload = randomize ();
 
 
 
 
-var x = [1,2,3,4,5,6,7,8,9,10]
 
 
-x[2]= Math.random(1);
+// Math.random Math.floor * times the number you want minus 1 
+function randomize () {
+    a = Math.floor(Math.random() * 9 )+1;
+    b = Math.floor(Math.random() * 9 )+1;
+    c = Math.floor(Math.random() * 9 )+1;
+    d = Math.floor(Math.random() * 9 )+1;
+    e = Math.floor(Math.random() * 9 )+1;
 
-console.log(x[2])
-
+    console.log(a,b,c,d,e)
+    document.getElementById("numbergen").innerHTML = "#" + [a, b,c,d,e]
+}
 
